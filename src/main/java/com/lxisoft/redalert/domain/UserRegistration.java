0f44e0074879a6d41e120a1803b31f63a187e88b
Sarathkrishna S <sarathkrishna.s@lxisoft.com>
2018-09-27 21:18:15 +0530

    RA-5

--- src/main/java/com/lxisoft/redalert/domain/UserRegistration.java
+++ src/main/java/com/lxisoft/redalert/domain/UserRegistration.java
@@ -1,222 +1,153 @@
-package com.lxisoft.redalert.domain;
+package com.lxisoft.UserRegistration.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-import javax.persistence.*;
-
-import java.io.Serializable;
-import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Objects;
-
-/**
- * A UserRegistration.
- */
 @Entity
-@Table(name = "user_registration")
-public class UserRegistration implements Serializable {
+public class UserRegistration {
 
-    private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Long phone;
+	private String email;
+	private String bloodGroup;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	private String password;
+	private String confirmPassword;
 
-    @Column(name = "first_name")
-    private String firstName;
+	public String getFirstName() {
+		return firstName;
+	}
 
-    @Column(name = "last_name")
-    private String lastName;
+	public String getLastName() {
+		return lastName;
+	}
 
-    @Column(name = "phone")
-    private Long phone;
+	public Long getPhone() {
+		return phone;
+	}
 
-    @Column(name = "email")
-    private String email;
+	public String getEmail() {
+		return email;
+	}
 
-    @Column(name = "blood_group")
-    private String bloodGroup;
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
 
-    @Column(name = "created_time")
-    private Instant createdTime;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    @OneToMany(mappedBy = "userRegistration")
-    @JsonIgnore
-    private Set<Feed> comments = new HashSet<>();
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    @ManyToMany
-    @JoinTable(name = "user_registration_emergency_contact",
-               joinColumns = @JoinColumn(name="user_registrations_id", referencedColumnName="id"),
-               inverseJoinColumns = @JoinColumn(name="emergency_contacts_id", referencedColumnName="id"))
-    private Set<Contact> emergencyContacts = new HashSet<>();
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
-    public Long getId() {
-        return id;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public UserRegistration firstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
 
-    public UserRegistration lastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
+	@Override
+	public String toString() {
+		return "UserRegistration [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone
+				+ ", email=" + email + ", bloodGroup=" + bloodGroup + ", password=" + password + ", confirmPassword="
+				+ confirmPassword + "]";
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bloodGroup == null) ? 0 : bloodGroup.hashCode());
+		result = prime * result + ((confirmPassword == null) ? 0 : confirmPassword.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		return result;
+	}
 
-    public Long getPhone() {
-        return phone;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserRegistration other = (UserRegistration) obj;
+		if (bloodGroup == null) {
+			if (other.bloodGroup != null)
+				return false;
+		} else if (!bloodGroup.equals(other.bloodGroup))
+			return false;
+		if (confirmPassword == null) {
+			if (other.confirmPassword != null)
+				return false;
+		} else if (!confirmPassword.equals(other.confirmPassword))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		return true;
+	}
+	
 
-    public UserRegistration phone(Long phone) {
-        this.phone = phone;
-        return this;
-    }
-
-    public void setPhone(Long phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public UserRegistration email(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getBloodGroup() {
-        return bloodGroup;
-    }
-
-    public UserRegistration bloodGroup(String bloodGroup) {
-        this.bloodGroup = bloodGroup;
-        return this;
-    }
-
-    public void setBloodGroup(String bloodGroup) {
-        this.bloodGroup = bloodGroup;
-    }
-
-    public Instant getCreatedTime() {
-        return createdTime;
-    }
-
-    public UserRegistration createdTime(Instant createdTime) {
-        this.createdTime = createdTime;
-        return this;
-    }
-
-    public void setCreatedTime(Instant createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public Set<Feed> getComments() {
-        return comments;
-    }
-
-    public UserRegistration comments(Set<Feed> feeds) {
-        this.comments = feeds;
-        return this;
-    }
-
-    public UserRegistration addComments(Feed feed) {
-        this.comments.add(feed);
-        feed.setUserRegistration(this);
-        return this;
-    }
-
-    public UserRegistration removeComments(Feed feed) {
-        this.comments.remove(feed);
-        feed.setUserRegistration(null);
-        return this;
-    }
-
-    public void setComments(Set<Feed> feeds) {
-        this.comments = feeds;
-    }
-
-    public Set<Contact> getEmergencyContacts() {
-        return emergencyContacts;
-    }
-
-    public UserRegistration emergencyContacts(Set<Contact> contacts) {
-        this.emergencyContacts = contacts;
-        return this;
-    }
-
-    public UserRegistration addEmergencyContact(Contact contact) {
-        this.emergencyContacts.add(contact);
-        return this;
-    }
-
-    public UserRegistration removeEmergencyContact(Contact contact) {
-        this.emergencyContacts.remove(contact);
-        return this;
-    }
-
-    public void setEmergencyContacts(Set<Contact> contacts) {
-        this.emergencyContacts = contacts;
-    }
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        UserRegistration userRegistration = (UserRegistration) o;
-        if (userRegistration.getId() == null || getId() == null) {
-            return false;
-        }
-        return Objects.equals(getId(), userRegistration.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
-
-    @Override
-    public String toString() {
-        return "UserRegistration{" +
-            "id=" + getId() +
-            ", firstName='" + getFirstName() + "'" +
-            ", lastName='" + getLastName() + "'" +
-            ", phone=" + getPhone() +
-            ", email='" + getEmail() + "'" +
-            ", bloodGroup='" + getBloodGroup() + "'" +
-            ", createdTime='" + getCreatedTime() + "'" +
-            "}";
-    }
 }
